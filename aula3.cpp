b34708a52ef98dd43b15825b4feee5909c105d00
Filipe Oliveira <filipepinholiveira@gmail.com>
2023-11-06 19:47:35 +0000

    Testes CPP

---
+++ aula3.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+#define canal cout << "\nO Filipe é o maior\n\n";
+
+int main()
+{
+    int vidas, tiros, life;
+    string teste;
+    
+    cout << "Digite nome do personagem: ";
+    cin >> teste;
+    cout << "Quantas vidas? ";
+    cin >> vidas;
+    cout << "Quantos tiros? ";
+    cin >> tiros;
+    cout << "Quanta life? ";
+    cin >> life;
+
+    canal
+
+    //cout << teste << "\n" << vidas << endl;
+
+    return 0;
+}
