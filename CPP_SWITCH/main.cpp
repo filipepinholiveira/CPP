b7cbe9e784e834d27aa20ee4e084c842ced5b06c
Filipe Oliveira <filipepinholiveira@gmail.com>
2023-11-16 15:23:12 +0000

    Brincadeira clubes

--- CPP_SWITCH/main.cpp
+++ CPP_SWITCH/main.cpp
@@ -7,34 +7,72 @@ int main ()
     main:
 
     int Clube;
+    int numero_socio;
     std::string Nome;
-    std::cout << "Qual é o seu clube?\n [1]: FCP\n [2]: SLB\n [3]: SCP\n" << std::endl;
+    std::cout << "Qual é o seu clube?\n [1]: Sócio FCP\n [2]: Simpatizante FCP\n [3]: Sócio SLB\n [4]: Simpatizante SLB\n [5]: Sócio SCP\n [6]: Simpatizante SCP\n" << std::endl;
     std::cin >> Clube;
 
     switch (Clube)
     {
     case 1:
-        system ("clear");
-        std::cout << "\n\n\nOlá Super Dragao. Qual é o seu nome? " << std::endl;
-        std::cin >> Nome;
-        std::cout << "Obrigado " << Nome << ". Vamos Jogar!!!" << std::endl;
-        break;
     case 2:
         system ("clear");
-        std::cout << "\n\n\nOlá NN Boy. Qual é o seu nome? " << std::endl;
+        std::cout << "\n\n\nOlá Super Dragao. Qual é o seu nome? \n";
         std::cin >> Nome;
-        std::cout << "Obrigado " << Nome << ". Vamos Jogar!!!" << std::endl;
+        std::cout << "Obrigado " << Nome << ".\n";
+        switch (Clube)
+        {
+            case 1:
+                std::cout << "Introduzao seu número de sócio: \n";
+                std::cin >> numero_socio;
+                std::cout << "Obrigado. Numero " << numero_socio << " validado.\n";
+                system ("ls -l");
+                break;
+            case 2:
+                std::cout << "Área reservada a associados\n";
+                break;
+        }
         break;
     case 3:
+    case 4:
         system ("clear");
-        std::cout << "\n\n\nOlá Lagarto. Qual é o seu nome? " << std::endl;
+        std::cout << "\n\n\nOlá NN Boy. Qual é o seu nome? \n";
         std::cin >> Nome;
-        std::cout << "Obrigado " << Nome << ". Vamos Jogar!!!" << std::endl;
+        std::cout << "Obrigado " << Nome << ".\n";
+        switch (Clube)
+        {
+            case 3:
+                std::cout << "Introduzao seu número de sócio: \n";
+                std::cin >> numero_socio;
+                std::cout << "Obrigado. Numero " << numero_socio << " validado.\n";
+                system ("ls -l");
+                break;
+            case 4:
+                std::cout << "Área reservada a associados\n";
+                break;
+        }
         break;
-    case a:
-        
+    case 5:
+    case 6:
+        system ("clear");
+        std::cout << "\n\n\nOlá Lagarto. Qual é o seu nome? \n";
+        std::cin >> Nome;
+        std::cout << "Obrigado " << Nome << ".\n";
+        switch (Clube)
+        {
+        case 5:
+            std::cout << "Introduzao seu número de sócio: \n";
+            std::cin >> numero_socio;
+            std::cout << "Obrigado. Numero " << numero_socio << " validado.\n";
+            system ("ls -l");
+            break;
+        case 6:
+            std::cout << "Área reservada a associados\n";
+            break;
+        }
+        break;
     default:
-        std::cout << "Clube nao reconhecido" << std::endl;
+        std::cout << "Clube nao reconhecido\n";
         goto main;
     }
     
