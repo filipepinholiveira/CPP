7679b93d7ae7ccbcf77d9857d39f85f352bf1266
Filipe Andre Pinho De Oliveira <fpinho-d@c1r15s6.42porto.com>
2024-04-22 18:36:52 +0100

    42 ESTA QUASEgit add .! FALTA MESMO TROCAR AS CLASSES

--- CPP-EXERCICIOS/ex01_mas_com_classes/auxiliar.cpp
+++ CPP-EXERCICIOS/ex01_mas_com_classes/auxiliar.cpp
@@ -3,18 +3,44 @@
 
 void    printHeader()
 {
-    std::cout << " ***************************************************" << std::endl; 
-    std::cout << "                                                    " << std::endl;
-    std::cout << "   *******  *****  *****  *    *  **     *******    " << std::endl;
-    std::cout << "   *     *  *      *      * *  *  *  *   *     *    " << std::endl;
-    std::cout << "   *******  *  **  ***    *  * *  *   *  *******    " << std::endl;
-    std::cout << "   *     *  *   *  *      *   **  *   *  *     *    " << std::endl;
-    std::cout << "   *     *  *****  *****  *    *  ****   *     *    " << std::endl;
-    std::cout << "                                                    " << std::endl;
-    std::cout << " ***************************************************" << std::endl;
-    std::cout << "  ADD ()            SEARCH ()             EXIT ()  *" << std::endl;
-    std::cout << "****************************************************" << std::endl;
-}
+
+std::cout << "*********************************************************" << std::endl;
+std::cout << "**    _____     _    _     ____     _   _    ______    **" << std::endl; 
+std::cout << "**   |  __ \\   | |  | |   / __ \\   | \\ | |  |  ____|   **" << std::endl; 
+std::cout << "**   | |__) |  | |__| |  | |  | |  |  \\| |  | |__      **" << std::endl; 
+std::cout << "**   |  ___/   |  __  |  | |  | |  | . ` |  |  __|     **" << std::endl; 
+std::cout << "**   | |       | |  | |  | |__| |  | |\\  |  | |____    **" << std::endl; 
+std::cout << "**   |_|       |_|  |_|   \\____/   |_| \\_|  |______|   **" << std::endl; 
+std::cout << "**                                                     **" << std::endl; 
+std::cout << "**                                                     **" << std::endl; 
+std::cout << "**        ____      ____      ____     _  __           **" << std::endl; 
+std::cout << "**       |  _ \\    / __ \\    / __ \\   | |/ /           **" << std::endl; 
+std::cout << "**       | |_) |  | |  | |  | |  | |  | ' /            **" << std::endl; 
+std::cout << "**       |  _ <   | |  | |  | |  | |  |  <             **" << std::endl; 
+std::cout << "**       | |_) |  | |__| |  | |__| |  | . \\            **" << std::endl; 
+std::cout << "**       |____/    \\____/    \\____/   |_|\\_\\           **" << std::endl; 
+std::cout << "**                                                     **" << std::endl; 
+std::cout << "**                                                     **" << std::endl; 
+std::cout << "*********************************************************" << std::endl;
+std::cout << "**      ADD ()          SEARCH ()         EXIT ()      **" << std::endl;
+std::cout << "*********************************************************" << std::endl;
+
+
+
+
+
+//     std::cout << " ***************************************************" << std::endl; 
+//     std::cout << "                                                    " << std::endl;
+//     std::cout << "   *******  *****  *****  *    *  **     *******    " << std::endl;
+//     std::cout << "   *     *  *      *      * *  *  *  *   *     *    " << std::endl;
+//     std::cout << "   *******  *  **  ***    *  * *  *   *  *******    " << std::endl;
+//     std::cout << "   *     *  *   *  *      *   **  *   *  *     *    " << std::endl;
+//     std::cout << "   *     *  *****  *****  *    *  ****   *     *    " << std::endl;
+//     std::cout << "                                                    " << std::endl;
+//     std::cout << " ***************************************************" << std::endl;
+//     std::cout << "  ADD ()            SEARCH ()             EXIT ()  *" << std::endl;
+//     std::cout << "****************************************************" << std::endl;
+// }
 
 // void    choose_convert(std::string choose, int *choice)
 // {
@@ -25,7 +51,7 @@ void    printHeader()
 //     if (choose == "EXIT")
 //         *choice = 3;
 
-// }
+}
 
 int check_isprint(std::string arg)
 {
