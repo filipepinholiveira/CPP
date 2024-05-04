9936d4fe2aa7597b42b1747f7a502e02e45f0d5e
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-05-04 16:56:56 +0000

    home testes ex04

--- CPP-EXERCICIOS/MODULE-01/ex04/main.cpp
+++ CPP-EXERCICIOS/MODULE-01/ex04/main.cpp
@@ -4,14 +4,26 @@
 
 int main()
 {
+
+    // este exemplo serve para criar um ficheiro a partir
+    // de um imput do utilizador usando o ofstream
+
+    // std::string Ementa;
+    // std::cout << "Adicione Pedido. Digite FIM para sair" << std::endl;
+    // std::ofstream file("Ementa.txt");
+    // while (Ementa != "FIM")
+    // {
+    //     getline(std::cin, Ementa);
+    //     if (Ementa != "FIM")
+    //         file << Ementa << std::endl;
+    // }
+    // file.close();
+
+    std::ifstream file("TESTE.txt");
     std::string Ementa;
-    std::cout << "Adicione Pedido. Digite FIM para sair" << std::endl;
-    std::ofstream file("Ementa.txt");
-    while (Ementa != "FIM")
-    {
-        getline(std::cin, Ementa);
-        if (Ementa != "FIM")
-            file << Ementa << std::endl;
-    }
+    while(file)
+        file >> Ementa;
     file.close();
+    std::cout << "Tu comeste " << Ementa << std::endl;
+
 }
