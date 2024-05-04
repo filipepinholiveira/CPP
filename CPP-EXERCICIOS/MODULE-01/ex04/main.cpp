1d7eba4e99846d443bec5527f22863cdaee9d4f2
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-05-04 22:40:27 +0000

    RTP M01 ex04 Programa recebe ficheiro e duplia com .replace

--- CPP-EXERCICIOS/MODULE-01/ex04/main.cpp
+++ CPP-EXERCICIOS/MODULE-01/ex04/main.cpp
@@ -2,8 +2,30 @@
 #include <iostream>
 #include <fstream>
 
-int main()
+int main(int argc, char **argv)
 {
+    //(void) argv;
+    if (argc != 4)
+    {
+        std::cout << "Invalid imputs" << std::endl;
+        return(1);
+    }
+    std::ifstream file1(argv[1]);
+    if (!file1) 
+    {
+        std::cerr << "Erro ao abrir o arquivo de entrada." << std::endl;
+        return 1;
+    }
+    std::string outputFilename = std::string(argv[1]) + ".replace";
+    std::ofstream file2(outputFilename.c_str());
+    if (!file2) 
+    {
+        std::cerr << "Erro ao criar o arquivo de saída." << std::endl;
+        return 1;
+    }
+    file2 << file1.rdbuf();
+    file1.close();
+    file2.close();
 
     // este exemplo serve para criar um ficheiro a partir
     // de um imput do utilizador usando o ofstream
@@ -22,14 +44,14 @@ int main()
     // este exemplo serve para abrir um ficheiro e escrever
     // o seu conteudo
 
-    std::ifstream file("TESTE.txt");
-    std::string Ementa;
-    std::cout << "Ementa: " << std::endl;
-    while (!file.eof())
-    {
-        (getline(file, Ementa));
-        std::cout << Ementa << std::endl;
-    }
-    file.close();
+    // std::ifstream file("TESTE.txt");
+    // std::string Ementa;
+    // std::cout << "Ementa: " << std::endl;
+    // while (!file.eof())
+    // {
+    //     (getline(file, Ementa));
+    //     std::cout << Ementa << std::endl;
+    // }
+    // file.close();
 
 }
