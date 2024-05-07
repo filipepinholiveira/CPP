1c123da5b2e592d497c1ab7f582d52911fd0fd15
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-05-07 16:36:50 +0000

    42 M01 ex04 main cria copia .replace e substitui s1 por s2

--- CPP-EXERCICIOS/MODULE-01/ex04/main.cpp
+++ CPP-EXERCICIOS/MODULE-01/ex04/main.cpp
@@ -1,31 +1,78 @@
-
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char **argv)
+void replaceAll(std::string &str, const std::string &search, const std::string &new_word) 
 {
-    //(void) argv;
+    size_t pos = 0;
+    size_t searchLength = search.length();
+    size_t new_wordLength = new_word.length();
+    
+    while ((pos = str.find(search, pos)) != std::string::npos) 
+    {
+        if (pos == 0 || (str[pos - 1] == ' ' && (pos + new_wordLength == str.length() || str[pos + new_wordLength] == ' ')))
+        {
+            str = str.substr(0, pos) + new_word + str.substr(pos + searchLength);
+            pos += new_wordLength; // Avança na string de substituição
+        }
+        else
+        {
+            pos += searchLength; // Avança a posição para evitar encontrar a mesma ocorrência novamente
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{   
+    std::string original;
+    // verifica se tem o numero de args valido
     if (argc != 4)
     {
-        std::cout << "Invalid imputs" << std::endl;
-        return(1);
+        std::cout << "Invalid inputs" << std::endl;
+        return 1;
     }
+
+    // verifica se argv[1] e ficheiro valido
     std::ifstream file1(argv[1]);
     if (!file1) 
     {
         std::cerr << "Erro ao abrir o arquivo de entrada." << std::endl;
         return 1;
     }
+
+    // cria copia de ficheiro dado em argv[1]
     std::string outputFilename = std::string(argv[1]) + ".replace";
     std::ofstream file2(outputFilename.c_str());
+
+    // verifica se ficheiro copia foi criado com sucesso
     if (!file2) 
     {
         std::cerr << "Erro ao criar o arquivo de saída." << std::endl;
         return 1;
     }
-    file2 << file1.rdbuf();
+
+    // duplica conteudo de ficheiro para ficheiro copia
+    while (file1)
+    {
+        while (getline(file1, original)) 
+        {
+            replaceAll(original, argv[2], argv[3]); // funcao onde substitui s1 por s2
+            file2 << original << std::endl; // 
+
+        }
+
+    }
     file1.close();
     file2.close();
+    return 0;
+}
+
+
+
+
+
+
+    // std::cout << firstLine << std::endl;
+    // std::cout << secondLine << std::endl;
 
     // este exemplo serve para criar um ficheiro a partir
     // de um imput do utilizador usando o ofstream
@@ -53,5 +100,3 @@ int main(int argc, char **argv)
     //     std::cout << Ementa << std::endl;
     // }
     // file.close();
-
-}
