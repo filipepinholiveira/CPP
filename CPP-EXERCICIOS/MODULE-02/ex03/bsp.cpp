700181fd9162b1e5b216d81853b09b385f1d1192
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-05-23 11:11:58 +0000

    RTP CPP02 done ex03 with vertex and no triangle check

--- CPP-EXERCICIOS/MODULE-02/ex03/bsp.cpp
+++ CPP-EXERCICIOS/MODULE-02/ex03/bsp.cpp
@@ -17,39 +17,56 @@ float area_calc(Point const &a, Point const &b, Point const &c)
     return myabs(total_areaf);
 }
 
-
+bool    is_vertex(Point const a, Point const b, Point const c, Point const point)
+{
+    if (a.get_x() == point.get_x() && a.get_y() == point.get_y())
+        return true;
+    if (b.get_x() == point.get_x() && b.get_y() == point.get_y())
+        return true;
+    if (c.get_x() == point.get_x() && c.get_y() == point.get_y())
+        return true;
+    return false;
+}
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-
-
 
 float total_area = area_calc(a, b, c);
 float area_1 = area_calc(point, b, c);
 float area_2 = area_calc(a, point, c);
 float area_3 = area_calc(a, b, point);
 
-// std::cout << "Area total: " << total_area << std::endl;
-// std::cout << "A1: " << area_1 << std::endl;
-// std::cout << "A2: " << area_2 << std::endl;
-// std::cout << "A3: " << area_3 << std::endl;
-// std::cout << "Areas_sum: " << area_1 + area_2 + area_3 << std::endl;
+std::cout << "Area total: " << total_area << std::endl;
+std::cout << "A1: " << area_1 << std::endl;
+std::cout << "A2: " << area_2 << std::endl;
+std::cout << "A3: " << area_3 << std::endl;
+std::cout << "Areas_sum: " << area_1 + area_2 + area_3 << std::endl;
 
 if (total_area == (area_1 + area_2 + area_3))
     
     {
         if (area_1 > 0 && area_2 > 0 && area_3 > 0)
         {
-            //std::cout << "True: The point given it's inside" << std::endl;
+            std::cout << "True: The point given it's inside" << std::endl;
             return true;
         }
-        // else
-        // {
-        //     std::cout << "False: The point is in one of the egdes" << std::endl;
-        //     return false;
-        // }
+        else if(is_vertex(a, b, c, point))
+        {
+            std::cout << "False: The point is a vertex" << std::endl;
+            return false;
+        }
+        else
+        {
+            std::cout << "False: The point is in one of the egdes" << std::endl;
+            return false;
+        }
     }
-//std::cout << "False: The point given is outside" << std::endl;
+else if (total_area == 0)
+{
+    std::cout << "False: Given points won't built a triangle" << std::endl;
+    return false;
+}
+std::cout << "False: The point given is outside" << std::endl;
 return false;
 
 }
--- CPP-EXERCICIOS/MODULE-02/ex03/main.cpp
+++ CPP-EXERCICIOS/MODULE-02/ex03/main.cpp
@@ -5,10 +5,10 @@
 int main()
 {
     Point a(0.0f, 0.0f);
-    Point b(0.0f,5.0f);
-    Point c(5.0f,0.0f);
+    Point b(0.0f, 3.0f);
+    Point c(3.0f,0.0f);
 
-    Point d(1.0f,0.9f);
+    Point d(0.0f,3.0f);
 
     (bsp(a, b, c, d)) ? std::cout << "Inside\n" : std::cout << "Outside\n";
     
