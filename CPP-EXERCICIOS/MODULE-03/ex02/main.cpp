e66b1d01c254ea00ecfbef763bdb86ca57d31e63
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-05-29 18:12:12 +0000

    RTP CPP03 ex02 done

---
+++ CPP-EXERCICIOS/MODULE-03/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include "FragTrap.hpp"
+
+int main()
+{
+    // ClapTrap teste;
+    // ClapTrap teste2("Vitima");
+    // std::cout << teste << std::endl;
+    // std::cout << teste2 << std::endl;
+    // teste.attack(teste2.getName());
+    // teste2.takeDamage(teste.getAtack());
+    // teste2.beRepaired(20);
+    // std::cout << teste << std::endl;
+    // std::cout << teste2 << std::endl;
+    FragTrap teste3;
+    FragTrap teste4("Filipe");
+    teste4.highFivesGuys();
+    std::cout << teste3 << std::endl;
+    std::cout << teste4 << std::endl;
+}
