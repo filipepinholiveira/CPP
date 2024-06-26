6feca1652ad5ef604b5deae318abe0b0908ea43f
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-06-26 17:05:12 +0000

    42 Init CPP04

---
+++ CPP-EXERCICIOS/MODULE-04/ex00/animal.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class animal
+{
+private:
+    /* data */
+    
+    
+public:
+
+    // ********************************* CANONICAL FORM INIT *********************************
+
+    // Default constructor
+    animal(/* args */);
+
+    // Constructor with args
+
+
+    // Copy constructor
+
+
+    // assignment operator
+
+    // Destructor
+    ~animal();
+
+    // ********************************* CANONICAL FORM INIT *********************************
+
+
+
+    // ********************************* OTHER FORM INIT *********************************
+
+    // getters
+
+
+    // setters
+
+    // ********************************* OTHER FORM INIT *********************************
+
+};
+
+// object print overload
+
+
