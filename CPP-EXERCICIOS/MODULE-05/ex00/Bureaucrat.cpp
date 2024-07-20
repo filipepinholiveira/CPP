187e20d651b236013df2651bc1a8c36b26a3512a
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-07-20 10:49:19 +0000

    RTP CPP05 ex00

---
+++ CPP-EXERCICIOS/MODULE-05/ex00/Bureaucrat.cpp
@@ -0,0 +1,15 @@
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("No name set")
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+
+
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Default destructor called" << std::endl;
+}
---
+++ CPP-EXERCICIOS/MODULE-05/ex00/Bureaucrat.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+    /* data */
+    const std::string _name;
+
+public:
+    Bureaucrat();
+
+    ~Bureaucrat();
+};
+
+
