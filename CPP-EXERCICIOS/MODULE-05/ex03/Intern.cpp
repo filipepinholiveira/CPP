72c560d6e007bb52a522322576153d288574cdfd
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-08-26 13:42:34 +0000

    CPP05 ex03 feito

--- CPP-EXERCICIOS/MODULE-05/ex03/Intern.cpp
+++ CPP-EXERCICIOS/MODULE-05/ex03/Intern.cpp
@@ -2,12 +2,12 @@
 
 Intern::Intern()
 {
-    std::cout << "Default constructor called" << std::endl;
+    // std::cout << "Default constructor called" << std::endl;
 }
 
 Intern::~Intern()
 {
-    std::cout << "Default destructor called" << std::endl;
+    // std::cout << "Default destructor called" << std::endl;
 
 }
 
@@ -19,12 +19,85 @@ Intern &Intern::operator=(Intern const &source)
 {
     if (this != &source)
     {
-        //copy definition
+        *this = source;
     }
     return *this;
 }
 
-AForm * makeForm(std::string formName, std::string target);
+AForm * Intern::makeForm(std::string formName, std::string target)
 {
+    AForm *formCreated = NULL;
+    int choice = 10;
+    std::string formNames[] = {"ShrubberyCreation", "RobotomyRequest", "PresidentialPardon"};
+
+    if (!formName.empty())
+    {
+
+        // if(formName == "ShrubberyCreation")
+        //     choice = 0;
+        // else if(formName == "RobotomyRequest")
+        //     choice = 1;
+        // else if(formName == "PresidentialPardon")
+        //     choice = 2;
+
+        for (int i = 0; i < 3; i++) {
+            if (formName == formNames[i]) {
+                choice = i;
+                break; // Parar o loop após encontrar a correspondência
+            }
+        }
+
+        switch (choice)
+        {
+        case 0:
+            formCreated = createShrubberyCreationForm(target);
+            break;
+        case 1:
+            formCreated = createRobotomyRequestForm(target);
+            break;
+        case 2:
+            formCreated = createPresidentialPardonForm(target);
+            break;
+
+        
+        default:
+
+            if (choice == 10)
+                throw NoValidForm();
+        }
+    }
+    else
+    {
+                throw NoValidForm();
+    }
+        
+
     
+    std::cout << "Intern creates " << formName << std::endl;
+
+    return formCreated;
 }
+
+
+    AForm * Intern::createShrubberyCreationForm(std::string target)
+    {
+        return new ShrubberyCreationForm(target);
+    }
+
+    AForm * Intern::createRobotomyRequestForm(std::string target)
+    {
+        return new RobotomyRequestForm(target);
+    }
+
+    AForm * Intern::createPresidentialPardonForm(std::string target)
+    {
+        return new PresidentialPardonForm(target);
+    }
+
+    const char *    Intern::NoValidForm::what() const throw()
+    {
+        return "Not valid form\n";
+    }
+
+
+
--- CPP-EXERCICIOS/MODULE-05/ex03/Intern.hpp
+++ CPP-EXERCICIOS/MODULE-05/ex03/Intern.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <iostream>
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+class AForm;
 
 class Intern
 {
@@ -15,5 +20,14 @@ class Intern
         Intern &operator=(Intern const &source);
 
     AForm * makeForm(std::string formName, std::string target);
+    AForm * createShrubberyCreationForm(std::string target);
+    AForm * createRobotomyRequestForm(std::string target);
+    AForm * createPresidentialPardonForm(std::string target);
 
+    class NoValidForm : public std::exception
+    {
+    
+    public:
+        virtual const char* what() const throw();
+    };
 };
