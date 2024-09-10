8d08b264921c3d9b4992959a7ae7fe93b8ea3dc4
filipepinholiveira <filipepinholiveira@gmail.vom>
2024-09-10 15:16:38 +0100

    CPP08 ex00 redo

--- CPP-EXERCICIOS/MODULE-08/ex00/easyfind.hpp
+++ CPP-EXERCICIOS/MODULE-08/ex00/easyfind.hpp
@@ -6,15 +6,16 @@
 
 template <typename T>
 
-bool easyfind(T value1, int value2)
+std::list<int>::const_iterator easyfind(T value1, int value2)
 {
     std::list<int>::const_iterator              it;
     std::list<int>::const_iterator              ite = value1.end();
 
+    
     for (it = value1.begin(); it != ite; it++)
     {
         if (*it == value2)
-            return true;
+            return it;
     }
-    return false;
+    throw 
 }
--- CPP-EXERCICIOS/MODULE-08/ex00/main.cpp
+++ CPP-EXERCICIOS/MODULE-08/ex00/main.cpp
@@ -10,10 +10,23 @@ int main()
     lst1.push_back(17); 
     lst1.push_back(42);
 
+    try
+    {
+        /* code */
+        easyfind(lst1, 1000);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    /*
+    
     if (easyfind(lst1, 1000))
         std::cout << "Existe no array\n";
     else
          std::cout << "Nao existe no array\n";
 
     return 0;
+    */
 }
