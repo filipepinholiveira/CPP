022a7c75e1475b5815418d41404e9370dcd29521
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-09-26 12:22:53 +0000

    CPP09 ex01 RTP verifica numeros, x e +

--- CPP-EXERCICIOS/MODULE-09/ex01/main.cpp
+++ CPP-EXERCICIOS/MODULE-09/ex01/main.cpp
@@ -2,17 +2,17 @@
 
 bool containsOnlyDigits(const char *str) 
 {
-    int pointflag = 0;
+    //int pointflag = 0;
     int i = 1;
     while (str[i]) 
     {
-        if (str[i] == '.')
-            pointflag++;
-        if (pointflag > 1)
-            return false;
+        // if (str[i] == '.')
+        //     pointflag++;
+        // if (pointflag > 1)
+        //     return false;
         if (str[i] == ' ' || str[i] == '\t' || str[i] == '-' || str[i] == '.')
             i++;
-        if (!isdigit(str[i]))
+        if (!isdigit(str[i]) && str[i] != 'x' && str[i] != '+')
         {
             return false;
         }
@@ -25,7 +25,10 @@ bool containsOnlyDigits(const char *str)
 int main(int argc, char **argv)
 {
     if (argc > 2)
+        {
         std::cerr << "Can only acept one argument" << std::endl;
+        return 1;
+        }
 
     if (argv[1] == NULL)
     {
@@ -33,11 +36,11 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // if (containsOnlyDigits(argv[1]))
-    // {
-    //     std::cerr << "Need valid input" << std::endl;
-    //     return 1;
-    // }
+    if (!containsOnlyDigits(argv[1]))
+    {
+        std::cerr << "Need valid input" << std::endl;
+        return 1;
+    }
 
 
 
